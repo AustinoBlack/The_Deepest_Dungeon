75b65411ee41173248c953276efec03eb4874e54
Austin Black <102131707+AustinoBlack@users.noreply.github.com>
2024-07-18 11:38:29 -0400

    cleaned up old code

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,7 +4,6 @@ var mouse_sensitivity := 0.001
 var twist_input := 0.0
 var pitch_input := 0.0
 
-@onready var twist_pivot := $TwistPivot
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,17 +16,8 @@ func _process(delta):
 	input.x = Input.get_axis("move_left", "move_right")
 	input.z = Input.get_axis("move_forward", "move_backward")
 	
-	apply_central_force(twist_pivot.basis * input * 1200.0 * delta)
+	apply_central_force(input * 1200.0 * delta)
 
 	# Press Esc and mouse will appear again.
-	#if Input.is_action_just_pressed("ui_cancel"):
-	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	
-	$TwistPivot.rotate_y(twist_input)
-	
-
-func _unhandled_input(event: InputEvent):
-	if event is InputEventMouseMotion:
-		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-			twist_input = - event.relative.x * mouse_sensitivity
-			pitch_input = - event.relative.y * mouse_sensitivity
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
