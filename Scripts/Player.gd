752bd57a40bd3582f7edf122e65450a56105b1a7
Austin Black <102131707+AustinoBlack@users.noreply.github.com>
2024-07-21 00:38:34 -0400

    added rudimentary look at cursor on right click

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -10,6 +10,8 @@ const RUN_SPEED = 5
 const JUMP_VELOCITY = 4.5
 var walking = false
 var running = false
+var aiming = true
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -22,6 +24,8 @@ func _ready():
 	animation_player.set_blend_time("walk", "idle", 0.2)
 	animation_player.set_blend_time("run", "walk", 0.2)
 	animation_player.set_blend_time("walk", "run", 0.2)
+	animation_player.set_blend_time("idle", "aim_idle", 0.2)
+	animation_player.set_blend_time("aim_idle", "idle", 0.2)
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -36,18 +40,21 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
-	#look at cursor
-	var space_state = get_world_3d().direct_space_state
-	var mouse_position = get_viewport().get_mouse_position()
-	rayorigin = camera.project_ray_origin(mouse_position)
-	rayend = rayorigin + camera.project_ray_normal(mouse_position) * 2000
-	var query = PhysicsRayQueryParameters3D.create(rayorigin, rayend);
-	var intersection = space_state.intersect_ray(query)
-	if not intersection.is_empty():
-		var pos = intersection.position
-		#visuals.look_at(Vector3(pos.x, pos.y, pos.z), Vector3(0, 1, 0))
-
-	if direction:
+	#look at cursor - probably need to move this into the direction if statement eventually
+	if Input.is_action_pressed("Aim"): # if right click ( aiming )
+		var space_state = get_world_3d().direct_space_state
+		var mouse_position = get_viewport().get_mouse_position()
+		rayorigin = camera.project_ray_origin(mouse_position)
+		rayend = rayorigin + camera.project_ray_normal(mouse_position) * 2000
+		var query = PhysicsRayQueryParameters3D.create(rayorigin, rayend);
+		var intersection = space_state.intersect_ray(query)
+		if not intersection.is_empty():
+			var pos = intersection.position
+			visuals.look_at(Vector3(pos.x, 0.5, pos.z), Vector3(0, 0.5, 0))
+			aiming = true
+			animation_player.play("aim_idle")
+	
+	elif direction: # if moving
 		if Input.is_action_pressed("Sprint"):
 			velocity.x = direction.x * RUN_SPEED
 			velocity.z = direction.z * RUN_SPEED
@@ -69,8 +76,7 @@ func _physics_process(delta):
 		
 		visuals.rotation.y = lerp_angle(visuals.rotation.y, atan2(-direction.x, -direction.z), delta * 10)
 		
-		
-	else:
+	else: # you are not moving, default to idle
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 		
@@ -80,6 +86,10 @@ func _physics_process(delta):
 		
 		elif running:
 			running = false
+			animation_player.play("idle")
+			
+		elif aiming:
+			aiming = false
 			animation_player.play("idle")
 
 	move_and_slide()
